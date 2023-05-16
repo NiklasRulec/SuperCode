8f37504ba59564da1c2d1ced30158e35eb533330
NiklasRulec <niklasrulec.98@gmail.com>
2023-05-16 16:21:21 +0200

    upload

--- 16.05/dom level 2_7/main.js
+++ 16.05/dom level 2_7/main.js
@@ -7,14 +7,7 @@ btn.addEventListener("click", () => {
   let newItem = document.createElement("div");
   newItem.textContent = counter;
   newItem.classList.add("rechteck");
-  if (
-    counter == 0 ||
-    counter == 10 ||
-    counter == 20 ||
-    counter == 30 ||
-    counter == 40 ||
-    counter == 50
-  ) {
+  if (counter == 0 || counter % 10 == 0) {
     newItem.classList.add("weiss");
   }
   container.appendChild(newItem);
